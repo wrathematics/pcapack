6e70f927e33e8d0fedad087f717da7841c90742f
wrathematics <wrathematics@gmail.com>
2014-08-05 19:13:46 -0400

    svd tests and benchmarks

--- R/wrappers_svd.r
+++ R/wrappers_svd.r
@@ -2,13 +2,27 @@ LA_svd <- function(x, nu=min(m, n), nv=min(m, n))
 {
   m <- nrow(x)
   n <- ncol(x)
+  k <- min(m, n)
   
   if (!is.double(x))
     storage.mode(x) <- "double"
   
-  .Call("R_pcapack_svd", as.integer(nu), as.integer(nv), 
+  ret <- .Call("R_pcapack_svd", as.integer(nu), as.integer(nv), 
         as.integer(m), as.integer(n), x, 
         PACKAGE="pcapack")
   
+  if (nu)
+  {
+    if (nu != k && nu != m)
+      ret$u <- ret$u[, 1L:nu]
+  }
+  
+  if (nv)
+  {
+    if (nv != k && nv != n)
+      ret$vt <- ret$vt[1L:nv, ]
+  }
+  
+  return( ret )
 }
 
--- benchmark/svd.r
+++ benchmark/svd.r
@@ -1,11 +1,18 @@
+library(rbenchmark)
 library(pcapack)
+
 
 set.seed(1234)
 m <- 2000
 n <- 500
 x <- matrix(rnorm(m*n), m, n)
 
-system.time(mdl1 <- LA_svd(x))[3]
-system.time(mdl2 <- La.svd(x))[3]
+reps <- 5
+
+mine <- function() {mdl1 <<- LA_svd(x)}
+R <- function() {mdl2 <<- La.svd(x)}
+
+cols <- c("test", "replications", "elapsed", "relative")
+benchmark(mine(), R(), columns=cols, replications=reps)
 
 all.equal(mdl1, mdl2)
