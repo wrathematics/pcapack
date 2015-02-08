a7ea06ad1bb4525d759aaaca6144933daf752503
Drew Schmidt <schmidt@math.utk.edu>
2015-02-08 18:36:11 -0500

    clarity in svd benchmark

--- benchmark/cov.r
+++ benchmark/cov.r
@@ -1,7 +1,7 @@
 library(pcapack)
 library(rbenchmark)
 
-m <- 1e4
+m <- 1e5
 n <- 250
 x <- matrix(rnorm(m*n), m, n)
 
--- benchmark/svd.r
+++ benchmark/svd.r
@@ -3,16 +3,16 @@ library(pcapack)
 
 
 set.seed(1234)
-m <- 4000
+m <- 5000
 n <- 250
 x <- matrix(rnorm(m*n), m, n)
 
-reps <- 10
+reps <- 5
 
 mine <- function() {mdl1 <<- LA_svd(x)}
 R <- function() {mdl2 <<- La.svd(x)}
 
 cols <- c("test", "replications", "elapsed", "relative")
-benchmark(mine(), R(), columns=cols, replications=reps)
+benchmark(Me = mine(), R = R(), columns=cols, replications=reps)
 
 all.equal(mdl1, mdl2)
