db4dfd5a5793d30f5df171ecedd8c1a39797e901
wrathematics <wrathematics@gmail.com>
2015-02-08 04:24:52 -0500

    wrapped scale fun

---
+++ src/scale.c
@@ -0,0 +1,17 @@
+#include "pcapack.h"
+
+SEXP R_scale(SEXP centerx, SEXP scalex, SEXP x)
+{
+  R_INIT;
+  const int m = nrows(x), n = ncols(x);
+  int info = 0;
+  SEXP scaled;
+  
+  newRmat(scaled, m, n, "double");
+  memcpy(REAL(scaled), REAL(x), m*n*sizeof(double));
+  
+  info = pcapack_scale(INT(centerx), INT(scalex), m, n, REAL(scaled));
+  
+  R_END;
+  return scaled;
+}
