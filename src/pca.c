152470277e49e50bb39cc778350c3f4a936bd820
Drew Schmidt <schmidt@math.utk.edu>
2015-02-08 18:40:56 -0500

    fixed pca wrapper

--- src/pca.c
+++ src/pca.c
@@ -7,25 +7,33 @@
   setAttrib(Z, R_DimNamesSymbol, X);
 
 
-SEXP R_pca_svd(SEXP M, SEXP N, SEXP X, SEXP CENTER, SEXP SCALE, SEXP RETROT)
+SEXP R_pca_svd(SEXP X, SEXP CENTER, SEXP SCALE, SEXP RETROT)
 {
   R_INIT;
-  const int m = INT(M), n = INT(N);
-  bool retrot = (bool) INTEGER(RETROT)[0];
-  bool center = (bool) INTEGER(CENTER)[0];
-  bool scale = (bool) INTEGER(SCALE)[0];
   int info = 0;
-  
+  const int m = nrows(X), n = ncols(X);
+  const bool retrot = (bool) INT(RETROT);
+  const bool centerx = (bool) INT(CENTER);
+  const bool scalex = (bool) INT(SCALE);
   const int k = m<n?m:n;
-  
-  SEXP RET, RET_NAMES, SDEV, TROT;
+  double *x;
+  SEXP RET, RET_NAMES, SDEV, TROT, XRET;
   SEXP pcnames, dimnames;
   
   
   newRvec(SDEV, k, "double");
   newRmat(TROT, k, n, "double");
   
-  prcomp_svd_(&m, &n, DBLP(X), DBLP(SDEV), DBLP(TROT), &retrot, &center, &scale, &info);
+  if (retrot)
+  {
+    newRmat(XRET, m, n, "double"); // FIXME need to pass in mxn, return mxk
+    memcpy(DBLP(XRET), DBLP(X), m*n*sizeof(double));
+    x = DBLP(XRET);
+  }
+  else
+    x = DBLP(X);
+  
+  pcapack_prcomp_svd(centerx, scalex, retrot, m, n, x, DBLP(SDEV), DBLP(TROT));
   
 /*  if (info != 0)*/
 /*    error(_("info=%d from Lapack routine '%s'"), info, "dgesdd");*/
@@ -33,8 +41,15 @@ SEXP R_pca_svd(SEXP M, SEXP N, SEXP X, SEXP CENTER, SEXP SCALE, SEXP RETROT)
   pcnames = make_pca_default_colnames(n);
   setDimNames(dimnames, pcnames, TROT);
   
-  RET_NAMES = make_list_names(2, "sdev", "rotation");
-  RET = make_list(RET_NAMES, 2, SDEV, TROT);
+  if (retrot)
+  {
+    RET_NAMES = make_list_names(2, "sdev", "rotation", "x");
+    RET = make_list(RET_NAMES, 2, SDEV, TROT, XRET);
+  }
+  else
+    RET_NAMES = make_list_names(2, "sdev", "rotation");
+    RET = make_list(RET_NAMES, 2, SDEV, TROT);
+  
   
   R_END;
   return RET;
@@ -42,7 +57,8 @@ SEXP R_pca_svd(SEXP M, SEXP N, SEXP X, SEXP CENTER, SEXP SCALE, SEXP RETROT)
 
 
 
-
+// TODO
+#if 0
 SEXP R_pca_eigcov(SEXP M, SEXP N, SEXP X, SEXP RETROT)
 {
   R_INIT;
@@ -73,5 +89,5 @@ SEXP R_pca_eigcov(SEXP M, SEXP N, SEXP X, SEXP RETROT)
   R_END;
   return RET;
 } 
-
+#endif
 
