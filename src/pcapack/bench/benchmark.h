1d7114e29f3927235859ea4623dacd349c5b6a91
Drew Schmidt <schmidt@math.utk.edu>
2015-02-24 20:05:47 -0500

    even more fancy/ridiculous benchmarking macros

--- src/pcapack/bench/benchmark.h
+++ src/pcapack/bench/benchmark.h
@@ -4,28 +4,60 @@
 
 #include <stdio.h>
 #include <time.h>
+#include <sys/time.h>
 
-double __benchmark_seconds;
-clock_t __benchmark_start, __benchmark_end;
+static double systime_wall()
+{
+  struct timeval t;
+  gettimeofday(&t, NULL);
+  return (double)t.tv_sec + ((double) t.tv_usec) / 1000000.;
+}
+
+static double systime_cpu()
+{
+    return (double) clock() / CLOCKS_PER_SEC;
+}
+
+double __benchmark_wall_seconds, __benchmark_cpu_seconds;
+double __benchmark_wall_start, __benchmark_wall_end;
+double __benchmark_cpu_start, __benchmark_cpu_end;
+
+#define MAXPRINTLEN 20
+static void print_func_name(char *call)
+{
+  int i = 0;
+  putchar('\n');
+  while (call[i] != '(' && call[i] != '\0' && i<MAXPRINTLEN)
+    putchar(call[i++]);
+  
+  printf(":\n");
+}
 
 #define TIMEEXPR(foo) \
-  __benchmark_start = clock(); \
+  __benchmark_cpu_start = systime_cpu(); \
+  __benchmark_wall_start = systime_wall(); \
   foo; \
-  __benchmark_end = clock(); \
-  __benchmark_seconds = (double)(__benchmark_end - __benchmark_start) / CLOCKS_PER_SEC
+  __benchmark_cpu_end = systime_cpu(); \
+  __benchmark_wall_end = systime_wall(); \
+  __benchmark_cpu_seconds = __benchmark_cpu_end - __benchmark_cpu_start; \
+  __benchmark_wall_seconds = __benchmark_wall_end - __benchmark_wall_start;
+
+#define SHOWTIMES(foo) \
+  print_func_name(#foo); \
+  printf("\tcpu\twall\n"); \
+  printf("Total:\t%.3f\t%.3f\n", __benchmark_cpu_seconds, __benchmark_wall_seconds);
 
 #define SYSTIME(foo) \
-  TIMEEXPR(foo) \
-  printf("%s:\t%d.%d seconds\n", #foo, __benchmark_msec/1000, __benchmark_msec%1000)
+  TIMEEXPR(foo); \
+  SHOWTIMES(foo);
 
 #define REPEAT(foo, n) \
-  for (int __benchint_i=0; __benchint_i<n; __benchint_i++) foo;
+  for (volatile int __benchint_i=0; __benchint_i<n; __benchint_i++) foo;
 
 #define BENCHMARK(foo, n) \
   TIMEEXPR(REPEAT(foo, n)); \
-  printf("%s:\n", #foo); \
-  printf("    %.3f seconds total\n", __benchmark_seconds); \
-  printf("    %.3f seconds average (%d runs)\n", __benchmark_seconds/((double) n), n)
+  SHOWTIMES(foo); \
+  printf("Avg:  \t%.3f\t%.3f\n", __benchmark_cpu_seconds/n, __benchmark_wall_seconds/n);
 
 
 #endif
