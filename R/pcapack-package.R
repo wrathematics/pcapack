1bf290499f10176c81b163e3a69ea00810ceb41f
wrathematics <wrathematics@gmail.com>
2015-03-13 18:56:21 -0400

    skeleton documentation

---
+++ R/pcapack-package.R
@@ -0,0 +1,23 @@
+#' PCAPACK
+#' 
+#' PCAPACK is a set of utilities for performing principal components
+#' analysis (including by means of truncated svd) very efficiently.
+#' The package also contains efficient, related helper utilities
+#' such as a replacement for R's covariance calculator.
+#' 
+#' \tabular{ll}{ 
+#'    Package: \tab PCAPACK \cr 
+#'    Type: \tab Package \cr 
+#'    License: \tab MPL \cr 
+#'    Needs Compilation: \tab yes \cr
+#' } 
+#' 
+#' 
+#' @useDynLib PCAPACK, R_cov, R_scale
+#' 
+#' @name PCAPACK-package
+#' @docType package
+#' @author Drew Schmidt
+#' @keywords Package
+NULL
+
