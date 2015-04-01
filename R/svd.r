24f9fb19d03bfb69a59d5b831ce83c4b9ce76148
wrathematics <wrathematics@gmail.com>
2015-04-01 19:09:53 -0400

    fixed R bindings for svd

--- R/svd.r
+++ R/svd.r
@@ -1,4 +1,43 @@
-SVD <- function(x, nu=min(n, p), nv=min(n, p))
+#' SVD
+#' 
+#' Singular value decomposition.
+#' 
+#' @param x
+#' The input matrix or dataframe.
+#' @param nu
+#' Number of left singular vectors to return.
+#' @param nv
+#' Number of right singular vectors to return.
+#' 
+#' @rdname cov
+#' @export
+svd2 <- function(x, nu=min(n, p), nv=min(n, p))
 {
+  if (!is.matrix(x))
+    x <- as.matrix(x)
   
+  if (!is.double(x))
+    storage.mode(x) <- "double"
+  
+  n <- nrow(x)
+  p <- ncol(x)
+  
+  ret <- .Call("R_pcapack_svd", as.integer(nu), as.integer(nv), x, PACKAGE="pcapack")
+  
+  k <- min(dim(x))
+  
+  if (nu)
+  {
+    if (nu != k && nu != n)
+      ret$u <- ret$u[, 1L:nu]
+  }
+  
+  if (nv)
+  {
+    if (nv != k && nv != p)
+      ret$vt <- ret$vt[1L:nv, ]
+  }
+  
+  return( ret )
 }
+
