882dac86056c12b43d59053df876597219b1b5ae
wrathematics <wrathematics@gmail.com>
2015-04-24 17:25:03 -0400

    better lapak headers, authorship

--- src/pcapack/src/lapack.h
+++ src/pcapack/src/lapack.h
@@ -15,7 +15,9 @@ void daxpy_(int *n, double *a, double *x, int *incx, double *y, int *incy);
 void dgemv_(const char *trans, const int *m, const int *n, const double *alpha, 
             const double *a, const int *lda, const double *x, const int *incx, 
             const double *beta, double *y, const int *incy);
-void dger_(int *m, int *n, double *alpha, double *x, int *incx, double *y, int *incy, double *a, int *lda);
+void dger_(const int *m, const int *n, const double *alpha, const double *x, 
+           const int *incx, const double *y, const int *incy, double *a, 
+           const int *lda);
 
 void dsyrk_(const char *uplo, const char *trans, const int *n, const int *k, 
             const double *alpha, const double *a, const int *lda, 
@@ -30,14 +32,21 @@ void dgemm_(const char *transa, const char *transb, const int *m, const int *n,
 // LAPCK
 void dlassq_(int *n, double *x, int *incx, double *scale, double *sumsq);
 
-void dgeqrf_(int *m, int *n, double *a, int *lda, double *tau, double *work, int *lwork, int *info);
-void dorgqr_(int *m, int *n, int *k, double *a, int *lda, double *tau, double *work, int *lwork, int *info);
+void dgeqrf_(const int *m, const int *n, double *restrict a, const int *lda, 
+             double *restrict tau, double *restrict work, const int *restrict lwork, 
+             int *info);
+void dorgqr_(const int *m, const int *n, const int *k, double *restrict a, 
+             const int *lda, double *tau, double *work, const int *lwork, int *info);
 
 void dgetrf_(int *m, int *n, double *a, int *lda, int *ipiv, int *info);
 void dgetri_(int *n, double *a, int *lda, int *ipiv, double *work, int *lwork, int *info);
 
-void dgesdd_(char *jobz, int *m, int *n, double *a, int *lda, double *s, double *u, int *ldu, double *vt, int *ldvt, double *work, int *lwork, int *iwork, int *info);
-void dsyevd_(char *jobz, char *uplo, int *n, double *a, int *lda, double *w, double *work, int *lwork, int *iwork, int *liwork, int *info);
+void dgesdd_(const char *jobz, const int *m, const int *n, double *a, 
+             const int *lda, double *s, double *u, const int *ldu, double *vt, 
+             const int *ldvt, double *work, const int *lwork, int *iwork, int *info);
+void dsyevd_(const char *jobz, const char *uplo, const int *n, double *a, 
+             const int *lda, double *w, double *work, const int *lwork, int *iwork, 
+             const int *liwork, int *info);
 void dsyevr_(char *jobz, char *range, char *uplo, int *n, double *a, int *lda, double *vl, double *vu, int *il, int *iu, double *abstol, int *m, double *w, double *z, int *ldz, int *isuppz, double *work, int *lwork, int *iwork, int *liwork, int *info);
 void dgeev_(char *jobvl, char *jobvr, int *n, double *a, int *lda, double *wr, double *wi, double *vl, int *ldvl, double *vr, int *ldvr, double *work, int *lwork, int *info);
 
